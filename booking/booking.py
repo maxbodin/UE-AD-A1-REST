758cee15538e81f243c9931bd8711c562828feb5
Maxime Bodin <maxime.bodin1@etudiant.univ-lr.fr>
2024-09-29 23:10:46 +0200

    booking.py : Reformat file.

--- booking/booking.py
+++ booking/booking.py
@@ -9,13 +9,14 @@ PORT = 3201
 HOST = '0.0.0.0'
 
 with open('{}/databases/bookings.json'.format("."), "r") as jsf:
-   bookings = json.load(jsf)["bookings"]
+    bookings = json.load(jsf)["bookings"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % PORT)
+    app.run(host=HOST, port=PORT)
