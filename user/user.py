4e0451eae103b60b0f84a6077cce724ebcc2b929
Maxime Bodin <maxime.bodin1@etudiant.univ-lr.fr>
2024-09-30 00:17:26 +0200

    user.py : Reformat file.

--- user/user.py
+++ user/user.py
@@ -9,13 +9,14 @@ PORT = 3203
 HOST = '0.0.0.0'
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+    users = json.load(jsf)["users"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % PORT)
+    app.run(host=HOST, port=PORT)
